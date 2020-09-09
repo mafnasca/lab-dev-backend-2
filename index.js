432301179a645f91716d7707dc3f10be3ed46bf9
Invisioncoregit <mafnas_conan@invisioncore.com>
2020-09-09 13:12:44 -0400

    removed old demo data comments and added gitignore

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 
 app.use(cors());
 
-const PORT = 8080;
+const PORT = 8081;
 
 // Expose PORT 8080 
 app.listen(PORT, () => {
-    console.log(`The server has been started`);
+    console.log(`The server has been started on ${PORT}`);
 })
 
 // Root of the api
@@ -18,12 +18,12 @@ app.get('/api/', (_, res) => {
     res.send(`Welcome to the technical lab developer api`);
 })
 
-// Sends the list of all members to the UI
+// Sends the list of all principle to the UI
 app.get('/api/principles', (_, res) => {
     res.json(principles);
 })
 
-// Sends the UI the selected member 
+// Sends the UI the selected priniciple 
 app.get('/api/principles/:id', (req, res) => {
     const selectedPrinciple = principles.filter(principle => principle.id === Number(req.params.id));
     if (!selectedPrinciple) {
